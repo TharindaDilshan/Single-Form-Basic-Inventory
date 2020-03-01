ddb6bea032ebc1be42f66eba558716bda0e94ba5
Tharinda Dilshan Piyadasa <tharindad7@gmail.com>
2020-03-01 13:46:05 +0530

    Form Interface

---
+++ BasicInventory/Inventory.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace BasicInventory
+{
+    partial class Inventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inventory));
+            this.labelItemId = new System.Windows.Forms.Label();
+            this.textBoxItemId = new System.Windows.Forms.TextBox();
+            this.textBoxItemName = new System.Windows.Forms.TextBox();
+            this.labelItemName = new System.Windows.Forms.Label();
+            this.textBoxUnitPrice = new System.Windows.Forms.TextBox();
+            this.labelUnitPrice = new System.Windows.Forms.Label();
+            this.textBoxDate = new System.Windows.Forms.TextBox();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.textBoxSupplier = new System.Windows.Forms.TextBox();
+            this.labelSupplier = new System.Windows.Forms.Label();
+            this.textBoxQuantity = new System.Windows.Forms.TextBox();
+            this.labelQuantity = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.labelSearch = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelItemId
+            // 
+            this.labelItemId.AutoSize = true;
+            this.labelItemId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelItemId.Location = new System.Drawing.Point(88, 111);
+            this.labelItemId.Name = "labelItemId";
+            this.labelItemId.Size = new System.Drawing.Size(62, 20);
+            this.labelItemId.TabIndex = 0;
+            this.labelItemId.Text = "Item ID";
+            // 
+            // textBoxItemId
+            // 
+            this.textBoxItemId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxItemId.Location = new System.Drawing.Point(181, 108);
+            this.textBoxItemId.Name = "textBoxItemId";
+            this.textBoxItemId.ShortcutsEnabled = false;
+            this.textBoxItemId.Size = new System.Drawing.Size(250, 26);
+            this.textBoxItemId.TabIndex = 1;
+            // 
+            // textBoxItemName
+            // 
+            this.textBoxItemName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxItemName.Location = new System.Drawing.Point(643, 108);
+            this.textBoxItemName.Name = "textBoxItemName";
+            this.textBoxItemName.ShortcutsEnabled = false;
+            this.textBoxItemName.Size = new System.Drawing.Size(250, 26);
+            this.textBoxItemName.TabIndex = 3;
+            // 
+            // labelItemName
+            // 
+            this.labelItemName.AutoSize = true;
+            this.labelItemName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelItemName.Location = new System.Drawing.Point(550, 111);
+            this.labelItemName.Name = "labelItemName";
+            this.labelItemName.Size = new System.Drawing.Size(87, 20);
+            this.labelItemName.TabIndex = 2;
+            this.labelItemName.Text = "Item Name";
+            this.labelItemName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxUnitPrice
+            // 
+            this.textBoxUnitPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUnitPrice.Location = new System.Drawing.Point(1117, 108);
+            this.textBoxUnitPrice.Name = "textBoxUnitPrice";
+            this.textBoxUnitPrice.ShortcutsEnabled = false;
+            this.textBoxUnitPrice.Size = new System.Drawing.Size(250, 26);
+            this.textBoxUnitPrice.TabIndex = 5;
+            // 
+            // labelUnitPrice
+            // 
+            this.labelUnitPrice.AutoSize = true;
+            this.labelUnitPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUnitPrice.Location = new System.Drawing.Point(1024, 111);
+            this.labelUnitPrice.Name = "labelUnitPrice";
+            this.labelUnitPrice.Size = new System.Drawing.Size(77, 20);
+            this.labelUnitPrice.TabIndex = 4;
+            this.labelUnitPrice.Text = "Unit Price";
+            // 
+            // textBoxDate
+            // 
+            this.textBoxDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDate.Location = new System.Drawing.Point(1117, 174);
+            this.textBoxDate.Name = "textBoxDate";
+            this.textBoxDate.ShortcutsEnabled = false;
+            this.textBoxDate.Size = new System.Drawing.Size(250, 26);
+            this.textBoxDate.TabIndex = 11;
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDate.Location = new System.Drawing.Point(1024, 177);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(44, 20);
+            this.labelDate.TabIndex = 10;
+            this.labelDate.Text = "Date";
+            // 
+            // textBoxSupplier
+            // 
+            this.textBoxSupplier.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSupplier.Location = new System.Drawing.Point(643, 174);
+            this.textBoxSupplier.Name = "textBoxSupplier";
+            this.textBoxSupplier.ShortcutsEnabled = false;
+            this.textBoxSupplier.Size = new System.Drawing.Size(250, 26);
+            this.textBoxSupplier.TabIndex = 9;
+            // 
+            // labelSupplier
+            // 
+            this.labelSupplier.AutoSize = true;
+            this.labelSupplier.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSupplier.Location = new System.Drawing.Point(550, 177);
+            this.labelSupplier.Name = "labelSupplier";
+            this.labelSupplier.Size = new System.Drawing.Size(67, 20);
+            this.labelSupplier.TabIndex = 8;
+            this.labelSupplier.Text = "Supplier";
+            // 
+            // textBoxQuantity
+            // 
+            this.textBoxQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxQuantity.Location = new System.Drawing.Point(181, 174);
+            this.textBoxQuantity.Name = "textBoxQuantity";
+            this.textBoxQuantity.ShortcutsEnabled = false;
+            this.textBoxQuantity.Size = new System.Drawing.Size(250, 26);
+            this.textBoxQuantity.TabIndex = 7;
+            // 
+            // labelQuantity
+            // 
+            this.labelQuantity.AutoSize = true;
+            this.labelQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelQuantity.Location = new System.Drawing.Point(88, 177);
+            this.labelQuantity.Name = "labelQuantity";
+            this.labelQuantity.Size = new System.Drawing.Size(68, 20);
+            this.labelQuantity.TabIndex = 6;
+            this.labelQuantity.Text = "Quantity";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.BackColor = System.Drawing.Color.LimeGreen;
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdd.Location = new System.Drawing.Point(428, 252);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(112, 42);
+            this.buttonAdd.TabIndex = 12;
+            this.buttonAdd.Text = "ADD";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            this.buttonAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.BackColor = System.Drawing.Color.Orange;
+            this.buttonUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUpdate.Location = new System.Drawing.Point(588, 252);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(112, 42);
+            this.buttonUpdate.TabIndex = 13;
+            this.buttonUpdate.Text = "UPDATE";
+            this.buttonUpdate.UseVisualStyleBackColor = false;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackColor = System.Drawing.Color.Crimson;
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelete.Location = new System.Drawing.Point(750, 252);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(112, 42);
+            this.buttonDelete.TabIndex = 14;
+            this.buttonDelete.Text = "DELETE";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClear.Location = new System.Drawing.Point(914, 252);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(112, 42);
+            this.buttonClear.TabIndex = 15;
+            this.buttonClear.Text = "CLEAR";
+            this.buttonClear.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(114, 358);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1230, 477);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSearch.Location = new System.Drawing.Point(203, 326);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.ShortcutsEnabled = false;
+            this.textBoxSearch.Size = new System.Drawing.Size(337, 26);
+            this.textBoxSearch.TabIndex = 18;
+            // 
+            // labelSearch
+            // 
+            this.labelSearch.AutoSize = true;
+            this.labelSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSearch.Location = new System.Drawing.Point(110, 329);
+            this.labelSearch.Name = "labelSearch";
+            this.labelSearch.Size = new System.Drawing.Size(60, 20);
+            this.labelSearch.TabIndex = 17;
+            this.labelSearch.Text = "Search";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1408, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(34, 36);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Inventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1454, 847);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.labelSearch);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxDate);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.textBoxSupplier);
+            this.Controls.Add(this.labelSupplier);
+            this.Controls.Add(this.textBoxQuantity);
+            this.Controls.Add(this.labelQuantity);
+            this.Controls.Add(this.textBoxUnitPrice);
+            this.Controls.Add(this.labelUnitPrice);
+            this.Controls.Add(this.textBoxItemName);
+            this.Controls.Add(this.labelItemName);
+            this.Controls.Add(this.textBoxItemId);
+            this.Controls.Add(this.labelItemId);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Inventory";
+            this.Text = "Inventory";
+            this.Load += new System.EventHandler(this.Inventory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelItemId;
+        private System.Windows.Forms.TextBox textBoxItemId;
+        private System.Windows.Forms.TextBox textBoxItemName;
+        private System.Windows.Forms.Label labelItemName;
+        private System.Windows.Forms.TextBox textBoxUnitPrice;
+        private System.Windows.Forms.Label labelUnitPrice;
+        private System.Windows.Forms.TextBox textBoxDate;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.TextBox textBoxSupplier;
+        private System.Windows.Forms.Label labelSupplier;
+        private System.Windows.Forms.TextBox textBoxQuantity;
+        private System.Windows.Forms.Label labelQuantity;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Label labelSearch;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
